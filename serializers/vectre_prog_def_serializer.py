45dfd7dcb4478418d7cd91117d51b203bd863cea
d0cd <pranavsaig@gmail.com>
2021-05-28 17:42:48 -0700

    Bug fix for CFG nodes without instructions to execute

--- serializers/vectre_prog_def_serializer.py
+++ serializers/vectre_prog_def_serializer.py
@@ -22,13 +22,18 @@ class VectreProgDefSerializer:
 
     def serialize_cfg_node(self, node: angr.knowledge_plugins.cfg.CFGNode):
         header = f"ENTRY_{node.block_id}:"
-        fixed_strs = []
-        disasm_str = str(node.block.disassembly)
-        inst_strs = disasm_str.split('\n')
-        assert len(node.block.instruction_addrs) == len(inst_strs)
-        for (addr, inst_str) in zip(node.block.instruction_addrs, inst_strs):
-            decimal_addr_str = re.sub(r'^0x\d+', str(addr), inst_str).strip()
-            fixed_str = f"    {decimal_addr_str};"
-            fixed_strs.append(fixed_str)
-        body = '\n'.join(fixed_strs)
+        if node.block is not None:
+            fixed_strs = []
+            print(node.block_id,  type(node), type(node.block))
+
+            disasm_str = str(node.block.disassembly)
+            inst_strs = disasm_str.split('\n')
+            assert len(node.block.instruction_addrs) == len(inst_strs)
+            for (addr, inst_str) in zip(node.block.instruction_addrs, inst_strs):
+                decimal_addr_str = re.sub(r'^0x\d+', str(addr), inst_str).strip()
+                fixed_str = f"    {decimal_addr_str};"
+                fixed_strs.append(fixed_str)
+            body = '\n'.join(fixed_strs)
+        else:
+            body = ""
         return f"{header}\n{body}"
