40b3963a588ded36e94eb8bb315c7ce5bd70f5e7
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-06-05 09:22:23 -0700

    Platform def template

---
+++ serializers/templates/platform_def_template.py
@@ -0,0 +1,6 @@
+from string import Template
+
+platform_def_template = Template(""
+                                 "platform $PLATFORM_NAME {\n"
+                                 "$BODY"
+                                 "}")
