f310cdbdaa0c7484770a708482387e8f8247fbda
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-06-07 15:17:17 -0700

    Updated prog/inst serializers to accept multiple files

--- serializers/vectre_inst_def_serializer.py
+++ serializers/vectre_inst_def_serializer.py
@@ -1,28 +1,40 @@
 from .aarch64_disasm_processor import AArch64DisassemblyProcessor
 from .amd64_disasm_processor import AMD64DisassemblyProcessor
 
+from typing import List
+
 import angr
 
 
 class VectreInstDefSerializer:
-    angr_project: angr.Project
+    angr_projects: List[angr.Project]
 
-    def __init__(self, _project):
-        self.angr_project = _project
-        if self.angr_project.arch == angr.archinfo.ArchAArch64():
+    def __init__(self, _projects):
+        self.angr_projects = _projects
+
+        # All projects must have the same arch
+        assert len(_projects) > 0, "Need at least one ANGR project"
+        arch = self.angr_projects[0].arch
+        for proj in self.angr_projects:
+            if proj.arch != arch:
+                raise Exception("VectreInstDefSerializer must be initialized with a list of ANGR projects that all use the same architecture.")
+
+        if self.angr_projects[0].arch == angr.archinfo.ArchAArch64():
             self.disas_processor = AArch64DisassemblyProcessor()
-        elif self.angr_project.arch == angr.archinfo.ArchAMD64():
+        elif self.angr_projects[0].arch == angr.archinfo.ArchAMD64():
             self.disas_processor = AMD64DisassemblyProcessor()
         else:
             self.disas_processor = None
 
     def serialize_inst_def(self):
         bb_strs = []
-        cfg = self.angr_project.analyses.CFGFast()
-        nodes = cfg.graph.nodes()
-        for node in nodes:
-            if node.block is not None:
-                bb_strs.append(str(node.block.disassembly))
+        for proj in self.angr_projects:
+            cfg = proj.analyses.CFGFast()
+            nodes = cfg.graph.nodes()
+            for node in nodes:
+                if node.block is not None:
+                    bb_strs.append(str(node.block.disassembly))
         inst_str = "\n".join(bb_strs)
         serialized_spec = self.disas_processor.generate_inst_def_skeleton(inst_str)
+        print(serialized_spec)
         return serialized_spec
--- serializers/vectre_prog_def_serializer.py
+++ serializers/vectre_prog_def_serializer.py
@@ -3,33 +3,32 @@ from .aarch64_disasm_processor import AArch64DisassemblyProcessor
 from .amd64_disasm_processor import AMD64DisassemblyProcessor
 
 import angr
-import re
 
 
 class VectreProgDefSerializer:
-    angr_project: angr.Project
+    angr_projects: angr.Project
 
-    def __init__(self, _project):
-        self.angr_project = _project
-        if self.angr_project.arch == angr.archinfo.ArchAArch64():
+    def __init__(self, _projects):
+        self.angr_projects = _projects
+        if self.angr_projects.arch == angr.archinfo.ArchAArch64():
             self.disas_processor = AArch64DisassemblyProcessor()
-        elif self.angr_project.arch == angr.archinfo.ArchAMD64():
+        elif self.angr_projects.arch == angr.rchinfo.ArchAMD64():
             self.disas_processor = AMD64DisassemblyProcessor()
         else:
             self.disas_processor = None
 
-    def serialize_binary(self):
-        #obj = self.angr_project.loader.main_object
-        #print(obj.sections)
-
-        cfg = self.angr_project.analyses.CFGFast()
-        nodes = cfg.graph.nodes()
-        basic_blocks = []
-        for node in nodes:
-            basic_blocks.append(self.serialize_cfg_node(node))
-        prog_name = self.angr_project.filename.replace(".o", "").replace("/", "_").replace(".", "").replace("-","_")
-        bb_str = "\n".join(basic_blocks)
-        return prog_def_template.substitute(PROG_NAME=prog_name, BASIC_BLOCKS=bb_str)
+    def serialize_binaries(self):
+        specs = []
+        for proj in self.angr_projects:
+            cfg = proj.analyses.CFGFast()
+            nodes = cfg.graph.nodes()
+            basic_blocks = []
+            for node in nodes:
+                basic_blocks.append(self.serialize_cfg_node(node))
+            prog_name = self.angr_projects.filename.replace(".o", "").replace("/", "_").replace(".", "").replace("-", "_")
+            bb_str = "\n".join(basic_blocks)
+            specs.append(prog_def_template.substitute(PROG_NAME=prog_name, BASIC_BLOCKS=bb_str))
+        return "\n\n".join(specs)
 
     def serialize_cfg_node(self, node: angr.knowledge_plugins.cfg.CFGNode):
         header = f"ENTRY_{node.block_id}:"
