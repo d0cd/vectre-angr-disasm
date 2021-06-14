b3509bdcfd9f186c25725cce5220e4fc0a7287b9
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-06-14 16:56:33 -0700

    Add rule for tbnz instruction

--- serializers/aarch64_disasm_processor/normalize_conditional_instructions.py
+++ serializers/aarch64_disasm_processor/normalize_conditional_instructions.py
@@ -15,6 +15,12 @@ class NormalizeConditionalInstructions(Transformer):
             condition = operands.children[1].children[0].value
             operands.children = operands.children[:1]
             op.children[0].value = f"{op.children[0].value}_{condition}"
+        elif op.children[0].value == 'tbnz':
+            operands = args[2]
+            assert len(operands.children) == 3, "Unexpected number of operands for tbnz"
+            bit_index = operands.children[1].children[0].value
+            op.children[0].value = op.children[0].value + f"_{bit_index}"
+            operands.children = [operands.children[0], operands.children[2]]
         return Tree("line", args)
 
 
